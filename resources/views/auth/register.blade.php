6a117216ca8c06c1b028cef814437d0ee779475e
Gianluca Cecere <gianlucacecere1@gmail.com>
2024-05-09 15:10:15 +0200

    Implementato fortify

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,24 @@
+<x-layout>
+
+    <form method="POST" action="register">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Username:</label>
+            <input type="text" name="name" class="form-control" id="name">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" id="password">
+        </div>
+        <div class="mb-3">
+            <label for="password_confirmation" class="form-label">Conferma Password</label>
+            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</x-layout>
--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,3 +1,14 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
-</div>
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12 text-center">
+                <h1 class="fw-light">Vertically Centered Masthead Content</h1>
+                <p class="lead">A great starter layout for a landing page</p>
+                @guest
+                <a href="{{ route('login') }}" class="btn btn-info">Accedi</a>
+                <a href="{{ route('register') }}" class="btn btn-success">Registrati</a>
+                @endguest
+            </div>
+        </div>
+    </div>
+</header>
