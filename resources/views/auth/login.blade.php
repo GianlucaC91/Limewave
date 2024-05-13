a4b466623e70a48ae2b9b63bcf259a187b70e315
“Roberta <robertapicciariello@gmail.com>
2024-05-13 11:26:32 +0200

    Rivista pagina di login lato front end

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,13 @@
 <x-layout>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-10 col-md-8 col-xl-6 border-p bg-p shadow-sm p-4">
-                <form method="POST" action="login">
+
+    <div class="container mt-5 ">
+        <div class="row justify-content-center ">
+            <div class="col-10 col-md-8 col-xl-6 border-p bg-white shadow-sm p-4 log-1">
+                <h1 class="display-5 fw-bold text-body-emphasis lh-1 my-3 underline-colors">Accedi</h1>
+                <form method="POST" action="login" class="">
                     @csrf
                     <div class="mb-3">
-                        <label for="email" class="form-label"> Indirizzo email </label>
+                        <label for="email" class="form-label log-1"> Indirizzo email </label>
                         <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
                     </div>
                     <div class="mb-3">
@@ -13,14 +15,14 @@
                         <input type="password" name="password" class="form-control" id="password">
                     </div>
                     {{-- DECOMMENT WHEN IMPLEMENTED --}}
-                    {{-- <div class="form-check">
-                        <input name="remember" class="form-check-input" type="checkbox" value="" id="exampleCheck1">
+                     <div class="form-check">
+                        <input name="remember" class="form-check-input" type="checkbox" value="true" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">
                             Ricordami
                         </label>
-                    </div> --}}
+                    </div> 
                     <div class="text-end">
-                        <button type="submit" class="btn btn-accent w-25 shadow-sm">Accedi</button>
+                        <button type="submit" class="btn btn-accent btn-lg fw-bold pt-3 px-4 shadow btn-hero-login pb-3 mt-4">Accedi</button>
                     </div>
                 </form>
             </div>
