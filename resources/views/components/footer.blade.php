a71e3fae23f5eb7b5430428f3e104c39c5d65851
minovapore <minovapore@gmail.com>
2024-05-13 18:45:08 +0200

    footer sistemato

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,8 +1,8 @@
  
  <footer class="bg-s py-3 px-5 mx-0 px-0 container-fluid">
         <div class="row justify-content-between">
-          <div class="col-6 col-md-2 mb-3 text-a mt-3 mx-5 px-5">
-            <h5>Limewire</h5>
+          <div class="col-6 col-md-2 mb-3 text-a mt-3">
+            <h5>Limewave</h5>
             <ul class="nav flex-column">
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p  link-footer-custom">Chi siamo</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Lavori</a></li>
@@ -21,8 +21,8 @@
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Come funziona</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Verifica dell'articolo</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Notizie</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Limewire Pro</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Guuida Limewire Pro</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Limewave Pro</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Guuida Limewave Pro</a></li>
             </ul>
           </div>
     
@@ -30,7 +30,7 @@
             <h5>Assistenza</h5>
             <ul class="nav flex-column">
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Centro assistenza</a></li>
-              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Vnedere</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Vendere</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Acquistare</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Sicurezza</a></li>
               <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-p link-footer-custom">Affidabilità</a></li>
