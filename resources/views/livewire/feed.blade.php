9e04f68d04ff01ca363a7ab0e1cb849a6e45036a
minovapore <minovapore@gmail.com>
2024-05-15 12:38:40 +0200

    merge2

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -14,17 +14,20 @@
                     @foreach ($articles as $article)
                     @if ($article->is_accepted || $article->user->is_admin)
                     <div class="card col-3 rounded-4 shadow-sm mx-2 my-3" style="width: 18rem;">
-                        <p class="pt-2 ps-3 fw-bold d-flex align-items-center "> <a
+                        <p class="fw-bold d-flex align-items-center mt-2"> <a
                             href="{{ route('user.profile', ['user' => $article->user]) }}"><img src="{{Storage::url($article->user->img)}}" class="card-img avatars "></a> {{ $article->user->name }}</p>
-                            <img src="https://picsum.photos/20{{ $article->id }}" class="card-img-top rounded-0 "
+                            <img src="https://picsum.photos/20{{ $article->id }}" class="card-img-top rounded-3 my-0 img-card"
                             alt="...">
-                            <div class="card-body h-75">
+                            <div class="card-body">
                                 <h5 class="card-title">{{ Str::limit($article->title, 20)}}</h5>
                                 <p class="card-text">Categoria: {{ $article->category->name }}</p>
-                                <p class="card-text">Prezzo: {{ $article->price }} €</p>
                                 <p class="card-text">{{ Str::limit($article->body, 50) }}</p>
-                                <a href="{{ route('article.detail', compact('article')) }}"
-                                class="btn btn-accent rounded-0 fw-bold shadow">Dettaglio</a>
+                                <p class="card-text">Prezzo: {{ $article->price }} €</p>
+                                <div class="justify-content-between d-flex">
+                                    <a href="{{ route('article.detail', compact('article')) }}"
+                                    class="btn btn-accent rounded-3 fw-bold shadow">Dettaglio</a>
+                                        <span><i class="bi bi-suit-heart-fill fs-5 heart"></i></span>
+                                </div>
                             </div>
                         </div>       
                         @endif      
