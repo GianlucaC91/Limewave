ed29b7469f43ce0f1ed7861ad212b33d385bc52c
minovapore <minovapore@gmail.com>
2024-05-15 18:01:49 +0200

    card modificate + fatto fronted email

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -16,8 +16,11 @@
                     <div class="card col-3 rounded-4 shadow-sm mx-2 my-3" style="width: 18rem;">
                         <p class="fw-bold d-flex align-items-center mt-2"> <a
                             href="{{ route('user.profile', ['user' => $article->user]) }}"><img src="{{Storage::url($article->user->img)}}" class="card-img avatars "></a> {{ $article->user->name }}</p>
-                            <img src="https://picsum.photos/20{{ $article->id }}" class="card-img-top rounded-3 my-0 img-card"
-                            alt="...">
+                            <div class="overflow-hidden rounded-3">
+                                <img src="https://picsum.photos/20{{ $article->id }}" class="card-img-top rounded-3 my-0 img-card"
+                                alt="...">
+                            </div>
+
                             <div class="card-body">
                                 <h5 class="card-title">{{ Str::limit($article->title, 20)}}</h5>
                                 <p class="card-text">Categoria: {{ $article->category->name }}</p>
@@ -26,7 +29,7 @@
                                 <div class="justify-content-between d-flex">
                                     <a href="{{ route('article.detail', compact('article')) }}"
                                     class="btn btn-accent rounded-3 fw-bold shadow">Dettaglio</a>
-                                        <span><i class="bi bi-suit-heart-fill fs-5 heart"></i></span>
+                                        {{-- <span><i class="bi bi-suit-heart-fill fs-5 heart"></i></span> --}}
                                 </div>
                             </div>
                         </div>       
