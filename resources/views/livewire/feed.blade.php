0530e909365de6cfaa9ac8884b47307800f60f57
Gianluca Cecere <gianlucacecere1@gmail.com>
2024-05-15 22:33:14 +0200

    Aggiunta tabella Articoli Rifiutati e cambiata logica : Rifiutare gli annunci li sposta nella seconda tabella dove possono essere ripristinati o eliminati definitivamente

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -35,7 +35,7 @@
     </div>
 </div> --}}
     
-    
+    <div>
 <div class="container-fluid pt-md-5 hero">
     <div class="row justify-content-center ">
         <div class="col-12 col-md-11 col-xl-9">
@@ -167,5 +167,5 @@
         </div>
     </div>
 </div>
-    
+</div>
     
