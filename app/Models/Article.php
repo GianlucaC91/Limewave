9f22462d8bb89a292754fb79af5322f97b73a95e
DenisParise <dd.parise@gmail.com>
2024-05-15 10:05:42 +0200

    risolto problema scout e tnt (?)

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,18 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Article extends Model
 {
+    use Searchable;
     use HasFactory;
 
     protected $fillable =[
-        "title",
-        "price",
-        "body",
-        "user_id",
-        "category_id",
+        'title',
+        'price',
+        'body',
+        'user_id',
+        'category_id',
     ];
+    
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+        
+        $array['title'] = $this->title;
+        $array['body'] = $this->body;
+        $array['category'] = $this->category;
+
+        return $array;
+    }
 
     public function user()
     {
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::get('/article/create', [ArticleController::class, 'create'])->name('artic
 
 Route::get('/article/detail/{article:title}', [ArticleController::class, 'show'])->name('article.detail');
 
+// Search
+Route::get('/ricerca/annuncio', [PublicController::class, 'searchArticles'])->name('articles.search');
+
 
