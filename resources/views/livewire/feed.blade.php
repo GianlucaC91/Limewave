b2ce6e1b348d865d8d9a04b620365c769fa4720e
Andrea-Asaro <andrea.asaro.24@gmail.com>
2024-05-15 17:39:41 +0200

    iniziato a stilizzare cards nella pagina categoryShow, pagina resa responsive

--- resources/views/livewire/feed.blade.php
+++ resources/views/livewire/feed.blade.php
@@ -13,7 +13,7 @@
                     
                     @foreach ($articles as $article)
                     @if ($article->is_accepted || $article->user->is_admin)
-                    <div class="card col-3 rounded-4 shadow-sm mx-2 my-3" style="width: 18rem;">
+                    <div class="card col-3 rounded-4 shadow-sm mx-2 my-3 px-0" style="width: 18rem;">
                         <p class="pt-2 ps-3 fw-bold d-flex align-items-center "> <a
                             href="{{ route('user.profile', ['user' => $article->user]) }}"><img src="{{Storage::url($article->user->img)}}" class="card-img avatars "></a> {{ $article->user->name }}</p>
                             <img src="https://picsum.photos/20{{ $article->id }}" class="card-img-top rounded-0 "
@@ -24,7 +24,7 @@
                                 <p class="card-text">Prezzo: {{ $article->price }} €</p>
                                 <p class="card-text">{{ Str::limit($article->body, 50) }}</p>
                                 <a href="{{ route('article.detail', compact('article')) }}"
-                                class="btn btn-accent rounded-0 fw-bold shadow">Dettaglio</a>
+                                class="btn btn-accent rounded-0 fw-bold shadow">Dettagli</a>
                             </div>
                         </div>       
                         @endif      
