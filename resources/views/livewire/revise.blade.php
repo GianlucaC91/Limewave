78a3af7bef9e248dd080407e49cb9cc637e6d129
Andrea-Asaro <andrea.asaro.24@gmail.com>
2024-05-16 20:09:30 +0200

    tabella annunci recenti aggiornata con nuova struttura, design migliorato. altri dettagli migliorati, migliorata indentazione

--- resources/views/livewire/revise.blade.php
+++ resources/views/livewire/revise.blade.php
@@ -1,7 +1,9 @@
 
+<div>
 
 
-            <div class="container-fluid">
+
+        <div class="container-fluid">
             <div class="row justify-content-center ">
                 <div class="col-12 my-5">
                     <h2 class="display-6 fw-bold text-body-emphasis lh-1 mb-3 underline-colors">Annunci Recenti</h2>
@@ -21,23 +23,26 @@
                                     <th scope="col">Prezzo</th>
                                     <th scope="col">Descrizione</th>
                                     <th scope="col">Immagini</th>
-                                    <th colspan="3" scope="col">Azioni</th>
+                                    <th scope="col"></th>
+                                    <th scope="col">Azioni</th>
+                                    <th scope="col"></th>
+                                    {{-- <th colspan="3" scope="col">Azioni</th> --}}
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($pending_articles as $pending)
-                                <tr class="text-center">
+                                <tr class="text-start">
                                     <th scope="row" class="p-3">{{$loop->iteration}}</th>
                                     <td>{{$pending->id}}</td>
                                     <td>{{$pending->category->name}}</td>
-                                    <td>{{Str::limit($pending->user->name, 15)}}</td>
-                                    <td>{{Str::limit($pending->title, 15)}}</td>
-                                    <td>{{Str::limit($pending->price, 15)}}</td>
-                                    <td>{{Str::limit($pending->body, 20)}}</td>
+                                    <td>{{Str::limit($pending->user->name, 14)}}</td>
+                                    <td>{{Str::limit($pending->title, 14)}}</td>
+                                    <td>{{Str::limit($pending->price, 14)}}</td>
+                                    <td>{{Str::limit($pending->body, 14)}}</td>
                                     <td>
                                         <i class="bi bi-card-image"></i>
                                     </td>
-                                    <td colspan="3" class="d-flex" height="65px;">
+                                    {{-- <td colspan="3" class="d-flex" height="65px;">
                                         <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                             data-bs-target="#article{{$pending->id}}">
                                             Visualizza
@@ -53,7 +58,20 @@
                                             Accetta
                                         </button>
 
-                                    </td>
+                                    </td> --}}
+                                    <td><button type="button" class="btn py-2 mt-1 btn-primary" data-bs-toggle="modal"
+                                        data-bs-target="#article{{$pending->id}}">
+                                        Visualizza
+                                    </button></td>
+                                    <td><button wire:click='rejectArticle({{$pending}})' class="btn py-2 mt-1 btn-danger"
+                                        wire:confirm="Sei sicuro di voler rifiutare l'annuncio?">
+                                        Rifiuta
+                                    </button></td>
+                                    <td><button wire:click='acceptArticle({{$pending}})' class="btn py-2 mt-1 btn-success"
+                                        wire:confirm="Sei sicuro di voler accettare l'annuncio?">
+                                        Accetta
+                                    </button></td>
+
                                 </tr>
                                 {{-- article modal --}}
                                 <div class="modal fade" id="article{{$pending->id}}" tabindex="-1"
@@ -92,98 +110,105 @@
                         </table>
                     </div>
                 </div>
-
-
+            </div>
+        </div>
+        {{-- fine primo container  --}}
             {{-- Rejected Announcements Table --}}
-                <h2 class="text-center">Articoli Rifiutati</h2>
-                <div class="row justify-content-center">
-                    <div class="col-12 col-md-10">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Id Articolo</th>
-                                        <th scope="col">Utente</th>
-                                        <th scope="col">Titolo</th>
-                                        <th scope="col">Prezzo</th>
-                                        <th scope="col">Descrizione</th>
-                                        <th scope="col">Immagini</th>
-                                        <th colspan="3" scope="col">Azioni</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @forelse ($rejected_articles as $rejected)
-                                    <tr class="text-center">
-                                        <th scope="row" class="p-3">{{$loop->iteration}}</th>
-                                        <td>{{$rejected->id}}</td>
-                                        <td>{{Str::limit($rejected->user->name, 15)}}</td>
-                                        <td>{{Str::limit($rejected->title, 15)}}</td>
-                                        <td>{{Str::limit($rejected->price, 15)}}</td>
-                                        <td>{{Str::limit($rejected->body, 20)}}</td>
-                                        <td>
-                                            <i class="bi bi-card-image"></i>
-                                        </td>
-                                        <td colspan="3" class="d-flex">
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                                data-bs-target="#article{{$rejected->id}}">
-                                                Visualizza
-                                            </button>
+            <div class="container-fluid">
+                <div class="row justify-content-center ">
 
-                                            {{-- Aggiungi bottoni Elimina definitivamente e Ripristina --}}
-                                            @if(Auth::user()->is_revisor || Auth::user()->is_admin)
-                                            <button wire:click="deleteArticle({{ $rejected->id }})" class="btn btn-danger">
-                                                Elimina definitivamente
-                                            </button>
+                    <div class="col-12 my-5">
+                        <h2 class="display-6 fw-bold text-body-emphasis lh-1 mb-3 underline-colors">Annunci Rifiutati</h2>
+                    </div>
 
-                                            <button wire:click="restoreArticle({{ $rejected->id }})" class="btn btn-warning">
-                                                Ripristina
-                                            </button>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                    {{-- article modal --}}
-                                    <div class="modal fade" id="article{{$rejected->id}}" tabindex="-1"
-                                        aria-labelledby="articleLabel" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{$rejected->title}}</h1>
-                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                        aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <p class="card-text">Categoria: {{ $rejected->category->name }}</p>
-                                                    <p class="card-text">Prezzo: {{ $rejected->price }} €</p>
-                                                    <p class="card-text">{{ Str::limit($rejected->body, 50) }}</p>
+                        <div class="col-12 col-md-10">
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Id Articolo</th>
+                                            <th scope="col">Utente</th>
+                                            <th scope="col">Titolo</th>
+                                            <th scope="col">Prezzo</th>
+                                            <th scope="col">Descrizione</th>
+                                            <th scope="col">Immagini</th>
+                                            <th colspan="3" scope="col">Azioni</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($rejected_articles as $rejected)
+                                        <tr class="text-center">
+                                            <th scope="row" class="p-3">{{$loop->iteration}}</th>
+                                            <td>{{$rejected->id}}</td>
+                                            <td>{{Str::limit($rejected->user->name, 15)}}</td>
+                                            <td>{{Str::limit($rejected->title, 15)}}</td>
+                                            <td>{{Str::limit($rejected->price, 15)}}</td>
+                                            <td>{{Str::limit($rejected->body, 20)}}</td>
+                                            <td>
+                                                <i class="bi bi-card-image"></i>
+                                            </td>
+                                            <td colspan="3" class="d-flex">
+                                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                                    data-bs-target="#article{{$rejected->id}}">
+                                                    Visualizza
+                                                </button>
 
-                                                </div>
-                                                <div class="modal-footer">
-                                                    <button data-bs-dismiss="modal" wire:click='rejectArticle({{$rejected}})'                                            
-                                                        class="btn btn-danger">
-                                                        Rifiuta
-                                                    </button>
+                                                {{-- Aggiungi bottoni Elimina definitivamente e Ripristina --}}
+                                                @if(Auth::user()->is_revisor || Auth::user()->is_admin)
+                                                <button wire:click="deleteArticle({{ $rejected->id }})" class="btn btn-danger">
+                                                    Elimina definitivamente
+                                                </button>
 
-                                                    <button data-bs-dismiss="modal" wire:click='acceptArticle({{$rejected}})'                                            
-                                                        class="btn btn-success">
-                                                        Accetta
-                                                    </button>
+                                                <button wire:click="restoreArticle({{ $rejected->id }})" class="btn btn-warning">
+                                                    Ripristina
+                                                </button>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                        {{-- article modal --}}
+                                        <div class="modal fade" id="article{{$rejected->id}}" tabindex="-1"
+                                            aria-labelledby="articleLabel" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{$rejected->title}}</h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <p class="card-text">Categoria: {{ $rejected->category->name }}</p>
+                                                        <p class="card-text">Prezzo: {{ $rejected->price }} €</p>
+                                                        <p class="card-text">{{ Str::limit($rejected->body, 50) }}</p>
+
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button data-bs-dismiss="modal" wire:click='rejectArticle({{$rejected}})'                                            
+                                                            class="btn btn-danger">
+                                                            Rifiuta
+                                                        </button>
+
+                                                        <button data-bs-dismiss="modal" wire:click='acceptArticle({{$rejected}})'                                            
+                                                            class="btn btn-success">
+                                                            Accetta
+                                                        </button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    @empty
-                                    <tr>
-                                        <td colspan="8" class="text-center">Non ci sono annunci rifiutati.</td>
-                                    </tr>
-                                    @endforelse
-                                </tbody>
-                            </table>
+                                        @empty
+                                        <tr>
+                                            <td colspan="8" class="text-center">Non ci sono annunci rifiutati.</td>
+                                        </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
+                    
                 </div>
+            </div>
+            {{-- fine secondo container  --}}
 
 
-            </div>
-            </div>
-            
+</div>
