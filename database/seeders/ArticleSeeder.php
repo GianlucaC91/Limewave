e232777e9fc1b554169ef242fbc064baa6ce73d0
Gianluca Cecere <gianlucacecere1@gmail.com>
2024-05-22 21:24:25 +0200

    modifiche

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -15,24 +15,24 @@ class ArticleSeeder extends Seeder
     */
     public function run(): void
     {
-        $faker = Faker::create();
+        // $faker = Faker::create();
         
-        $categories = Category::all();
+        // $categories = Category::all();
         
-        foreach ($categories as $category) {
-            // $category_id = $category->id;
-            $num_announcements = rand(0, 8);
+        // foreach ($categories as $category) {
+        //     // $category_id = $category->id;
+        //     $num_announcements = rand(0, 8);
         
-            for ($i = 0; $i < $num_announcements; $i++) {
-                Article::create([
-                    'title' => $faker->words($faker->numberBetween(2, 10), true),
-                    'price' => $faker->randomFloat(2, 10, 1000),
-                    'body' => $faker->text(rand(30, 200)),
-                    'user_id' => rand(1, 4),
-                    'category_id' => $category->id,
-                    'is_accepted' => [0, 1, null][rand(0, 2)]
-                ]);
-            }
-        }
+        //     for ($i = 0; $i < $num_announcements; $i++) {
+        //         Article::create([
+        //             'title' => $faker->words($faker->numberBetween(2, 10), true),
+        //             'price' => $faker->randomFloat(2, 10, 1000),
+        //             'body' => $faker->text(rand(30, 200)),
+        //             'user_id' => rand(1, 4),
+        //             'category_id' => $category->id,
+        //             'is_accepted' => [0, 1, null][rand(0, 2)]
+        //         ]);
+        //     }
+        // }
     }
 }
